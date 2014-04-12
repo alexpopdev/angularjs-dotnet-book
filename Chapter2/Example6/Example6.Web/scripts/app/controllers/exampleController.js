522c5e250e0b7e0058b2e22d51c0c1905fbebe70
Alexandru Vasile Pop <palexandru@gmail.com>
2014-04-12 15:52:01 +0100

    Added Example6 with window.setTiemout alternatives

---
+++ Chapter2/Example6/Example6.Web/scripts/app/controllers/exampleController.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    "use strict";
+
+    var myAppModule = angular.module('myApp');
+
+    myAppModule.controller('ExampleController', ['$scope', '$timeout', 'playerService',
+            function ($scope, $timeout, playerService) {
+                $scope.reset = function () {
+                    $scope.players = null;
+                    playerService.resetPlayerCount();
+                }
+
+                $scope.callwindowsetTimeout = function () {
+                    $scope.players = [];
+                    window.setTimeout(function () {
+                        $scope.players = [playerService.createPlayer(), playerService.createPlayer()];
+                        playerService.resetPlayerCount();
+                        console.log($scope.players.length.toString() + " players were created.");
+                    }, 2000);
+                };
+
+                $scope.call$timeout = function () {
+                    $scope.players = [];
+                    $timeout(function () {
+                        $scope.players = [playerService.createPlayer(), playerService.createPlayer()];
+                        playerService.resetPlayerCount();
+                    }, 2000);
+                };
+
+                $scope.callwindowsetTimeoutWithin$scope$apply = function () {
+                    $scope.players = [];
+                    window.setTimeout(function () {
+                        $scope.$apply(function () {
+                            $scope.players = [playerService.createPlayer(), playerService.createPlayer()];
+                            playerService.resetPlayerCount();
+                        });
+                    }, 2000);
+                };
+            }
+    ]);
+})();
