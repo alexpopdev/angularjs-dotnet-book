a1891b47b68532dd30dc791f9925af5ed5c553ea
Alexandru Vasile Pop <palexandru@gmail.com>
2014-04-13 11:28:57 +0100

    removed code to simplify the example

--- Chapter2/Example7/Example7.Web/scripts/app/app.js
+++ Chapter2/Example7/Example7.Web/scripts/app/app.js
@@ -4,11 +4,9 @@
     var myAppModule = angular.module('myApp', ['ngRoute']);
 
     myAppModule.config([
-        '$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        '$routeProvider', function ($routeProvider) {
             $routeProvider.when('/singleplayer', { templateUrl: 'scripts/app/views/singlePlayer.html' });
             $routeProvider.when('/multiplayer', { templateUrl: 'scripts/app/views/multiPlayer.html' });
-
-           //$locationProvider.html5Mode(true);
         }
     ]);
 
