4e632e5f0f781b8735ba57a6e870b83e937d2d3f
Alex Pop <palexandru@gmail.com>
2014-04-18 23:36:56 +0100

    Chapter3 Example2: the first web service

---
+++ Chapter3/Example2/Example2/AppHost.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack.WebHost.Endpoints;
+
+namespace Example2
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("Chapter3 Example2", typeof (AppHost).Assembly)
+        {
+        }
+
+        public override void Configure(Funq.Container container)
+        {
+        }
+    }
+}
---
+++ Chapter3/Example2/Example2/PlayersService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ServiceStack.ServiceHost;
+
+namespace Example2
+{
+    public class PlayersService : IService
+    {
+        public List<Player> Get(GetPlayersRequest request)
+        {
+            return GetPlayers();
+        }
+
+        public Player Get(GetPlayerRequest request)
+        {
+            return GetPlayers().Single(p => p.Id == request.Id);
+        }
+
+        private List<Player> GetPlayers()
+        {
+            return new List<Player>
+            {
+                new Player{ Id= 1, FirstName="Angela", LastName = "Evans"},
+                new Player{ Id= 2, FirstName="Jack", LastName = "Marshall"},
+            };
+        }
+    }
+
+    [Route("/players", "GET")]
+    public class GetPlayersRequest : IReturn<List<Player>>
+    {
+    }
+
+    [Route("/players/{Id}", "GET")]
+    public class GetPlayerRequest : IReturn<Player>
+    {
+        public int Id { get; set; }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
