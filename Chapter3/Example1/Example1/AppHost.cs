b631159e2b222563d0c934f6a6989de2c5674365
Alex Pop <palexandru@gmail.com>
2014-04-18 21:42:24 +0100

    Chapter 3 Example 1: getting started with ServiceStack

---
+++ Chapter3/Example1/Example1/AppHost.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack.WebHost.Endpoints;
+
+namespace Example1
+{
+    public class AppHost : AppHostHttpListenerBase
+    {
+        public AppHost()
+            : base("Chapter3 Example1", typeof (AppHost).Assembly)
+        {
+        }
+
+        public override void Configure(Funq.Container container)
+        {
+
+        }
+    }
+}
