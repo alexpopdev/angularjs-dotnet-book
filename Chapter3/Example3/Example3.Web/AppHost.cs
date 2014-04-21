4d838e7ca721a809323baabbb38a8cd45aaa96e5
Alexandru Vasile Pop <palexandru@gmail.com>
2014-04-21 15:56:01 +0100

    added Example3: request filter

---
+++ Chapter3/Example3/Example3.Web/AppHost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ServiceStack.WebHost.Endpoints;
+
+namespace Example2.Web
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("Chapter3 Example2", typeof (AppHost).Assembly)
+        {
+        }
+
+        public override void Configure(Funq.Container container)
+        {
+            RequestFilters.Add((request, response, requestData) =>
+            {
+                if(!request.IsSecureConnection)
+                {
+                    throw new Exception("The application needs to be secured. Use https instead of http for hosting!");
+                }
+            });
+        }
+    }
+}
